5a1a4f8fac90e1828e37ef5bcc291302aacaf297
forzups <forzups@gmail.com>
2026-08-17 01:38:54 +0300

    Rename ExampleProvider to AniziumProvider

---
+++ ExampleProvider/src/main/kotlin/com/example/AniziumProvider.kt
@@ -0,0 +1,12 @@
+package com.lagradost
+
+import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.MainAPI
+
+class AniziumProvider : MainAPI() {
+    override var mainUrl = "https://anizium.com"
+    override var name = "Anizium"
+    override var hasMainPage = true
+    override var lang = "tr"
+    override var supportedTypes = setOf(TvType.Anime)
+}
