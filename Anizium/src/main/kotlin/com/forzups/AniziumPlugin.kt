d055ab19897fd19a56d1a2637bda1adf7ac02e4e
forzups <forzups@gmail.com>
2026-08-18 02:00:15 +0300

    Remove commented validation lines in AniziumPlugin

--- Anizium/src/main/kotlin/com/forzups/AniziumPlugin.kt
+++ Anizium/src/main/kotlin/com/forzups/AniziumPlugin.kt
@@ -3,8 +3,6 @@ package com.kerimmkirac
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
-// manifest.json içinden doğrulandı:
-// "pluginClassName": "com.kerimmkirac.AniziumPlugin", "name": "Anizium"
 @CloudstreamPlugin
 class AniziumPlugin : BasePlugin() {
     override fun load() {
